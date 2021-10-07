175390a61f6bdf0fb6003d9b0af1483dcea4e987
unknown <Kondratowicz1983@gmail.com>
2021-10-08 00:38:35 +0200

    list of pdfs

--- src/app.js
+++ src/app.js
@@ -8,10 +8,44 @@
     try {
         const response = await fetch('config.json');
          const data = await response.json();
+
          const company  = data.settings.company
          document.getElementById("company").innerHTML = company;
+
+        const pdfList = data.assets.pdfs
+        const final = JSON.stringify(pdfList)
+        document.getElementById("pdf").innerHTML = pdfList;
+        const pdflistObjects = pdfList.forEach(function(item, index, array) {
+          const namePdf = item.title;
+
+          function addElement() {
+            // tworzy nowy element div
+            // i daje jego zawartość
+            
+            newDiv = document.createElement("div");
+            newDiv.innerHTML = "<h1>Hi there and greetings! </h1>" + namePdf;
+            console.log("jest");
+
+            // add the newly created element and it's content into the DOM do sprawdzenia
+            my_div = document.getElementById("org_div1");
+            document.body.insertBefore(newDiv, my_div);
+          }
+          addElement()
+          console.log(item.title, index);
+        return item.title;
+        })
+        document.getElementById("produkty").innerHTML = pdflistObjects;
+
         // enter you logic when the fetch is successful
          console.log(company);
+         console.log(pdfList[0].title);
+         console.log(pdflistObjects);
+         console.log(final);
+         console.log(pdfList.lenght);
+
+
+
+
        } catch(error) {
     // enter your logic for when there is an error (ex. error toast)
           console.log(error)
